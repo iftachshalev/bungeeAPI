7ab44251a315bdc1dd42f6d98c38c193e4481f9a
Iftach Shalev <iftachshalev30115@gmail.com>
2020-03-28 23:22:40 +0300

    lern git

---
+++ IO_Class.py
@@ -0,0 +1,14 @@
+
+
+class IO_Class:
+
+    def __init__(self):
+        self.file_flag = None
+        self.screen_flag = None
+        self.URL = None
+        # self.file = open(self.URL, "W")
+
+    def print(self, what_to_print):
+        pass
+    def close(self):
+        pass
