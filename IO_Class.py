a01a1f9fe8af93a97643a05e97b738bf08951c77
Iftach Shalev <iftachshalev30115@gmail.com>
2020-03-29 11:47:36 +0300

    finish 'IO_Class'

--- IO_Class.py
+++ IO_Class.py
@@ -5,10 +5,17 @@ class IO_Class:
     def __init__(self):
         self.file_flag = None
         self.screen_flag = None
-        self.URL = None
-        # self.file = open(self.URL, "W")
+        self.url = ""
+        self.file = open(self.url, "W")
 
     def print(self, what_to_print):
-        pass
+        if self.file_flag and self.screen_flag:
+            print(what_to_print)
+            self.file.write(what_to_print)
+        elif self.file_flag and not self.screen_flag:
+            self.file.write(what_to_print)
+        elif self.screen_flag and not self.file_flag:
+            print(what_to_print)
+
     def close(self):
-        pass
+        self.file.close()
