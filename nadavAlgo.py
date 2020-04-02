ce6b276f5e197a68b2835ae0b24b7b93f91aaed9
nadav shalev <nadav.shalev@outlook.com>
2020-04-02 17:55:09 +0300

    edit nadavAlgo

--- nadavAlgo.py
+++ nadavAlgo.py
@@ -1,4 +1,7 @@
 
+def arr2str(arr):
+    return ''.join(str(arr).strip('[]').split(', '))
+
 def algo_simple(my_cards, lucky_card, lost_card, bungee_mode, score):
     last_ind = len(my_cards)-1
 
@@ -8,12 +11,18 @@ def algo_simple(my_cards, lucky_card, lost_card, bungee_mode, score):
     if score <= 5:
         return 'B'
 
-    for card in my_cards:
+    for i, card in enumerate(my_cards):
         if card == lost_card:
             from_stack = 'F'
+            if throw_card == i:
+                throw_card -= 1
 
     if len(my_cards) > 2 and[-1] == my_cards[-2]:
         throw_card = [last_ind-1, last_ind]
 
-    throw_str = ''.join(str(throw_card).strip('[]').split(', '))
+    throw_str = arr2str(throw_card)
     return throw_str + from_stack
+
+
+print(algo_simple([2,3,5,7,8], 9, 8, False, 25))
+print(algo_simple([2,3,5,8,8], 9, 7, False, 26))
