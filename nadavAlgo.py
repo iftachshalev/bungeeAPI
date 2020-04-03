19513a8b2074c593c9f17c0243539f583eca78d3
nadav shalev <nadav.shalev@outlook.com>
2020-04-03 16:56:39 +0300

    change my algo to numarate

--- nadavAlgo.py
+++ nadavAlgo.py
@@ -2,27 +2,34 @@
 def arr2str(arr):
     return ''.join(str(arr).strip('[]').split(', '))
 
+
 def algo_simple(my_cards, lucky_card, lost_card, bungee_mode, score):
     last_ind = len(my_cards)-1
 
-    from_stack = 'T'
+    from_stack = True
+    say_bungee = False
     throw_card = last_ind
 
     if score <= 5:
-        return 'B'
+        say_bungee = True
 
     for i, card in enumerate(my_cards):
         if card == lost_card:
-            from_stack = 'F'
+            from_stack = False
             if throw_card == i:
                 throw_card -= 1
 
-    if len(my_cards) > 2 and[-1] == my_cards[-2]:
-        throw_card = [last_ind-1, last_ind]
+    if len(my_cards) > 2 and my_cards[-1] == my_cards[-2]:
+        throw_card = [throw_card-1, throw_card]
 
-    throw_str = arr2str(throw_card)
-    return throw_str + from_stack
+    dict = {
+        'say_bungee': say_bungee,
+        'from_stack': from_stack,
+        'throw_cards': throw_card
+    }
+
+    return dict
 
 
-# print(algo_simple([2, 3, 5, 7, 8], 9, 8, False, 25))
-# print(algo_simple([2, 3, 5, 8, 8], 9, 7, False, 26))
+print(algo_simple([2, 3, 5, 7, 8], 9, 8, False, 25))
+print(algo_simple([2, 3, 5, 8, 8], 9, 7, False, 26))
