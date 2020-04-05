b625778024879b912a19ad2cdae7b31fedd377f9
Iftach Shalev <iftachshalev30115@gmail.com>
2020-04-06 01:05:41 +0300

    iftachAlgo

--- iftachAlgo.py
+++ iftachAlgo.py
@@ -6,7 +6,7 @@ def iftach_algo_simple(my_cards, lucky_card, lost_card, bungee_mode, score):
     say_bungee = False
     throw_card = []
     to_quit = False
-    eror = ""
+    error = ""
     from_stack = True
 
     luc = lucky_card
@@ -21,7 +21,7 @@ def iftach_algo_simple(my_cards, lucky_card, lost_card, bungee_mode, score):
             'say_bungee': say_bungee,
             'from_stack': from_stack,
             'quit': to_quit,
-            'error': eror,
+            'error': error,
             'throw_cards': throw_card
             }
             return dict
@@ -31,7 +31,7 @@ def iftach_algo_simple(my_cards, lucky_card, lost_card, bungee_mode, score):
             'say_bungee': say_bungee,
             'from_stack': from_stack,
             'quit': to_quit,
-            'error': eror,
+            'error': error,
             'throw_cards': throw_card
         }
         return dict
@@ -40,8 +40,13 @@ def iftach_algo_simple(my_cards, lucky_card, lost_card, bungee_mode, score):
     is_bungee = if_to_say_bungee(my_cards, lost_card, bungee_mode, score, index_best_array)
 
     if bungee_mode:
-        pass
-
+        if not is_bungee:
+            to_quit = True
+        elif is_bungee:
+            say_bungee = True
+        if is_bungee == None:
+            from_stack = False
+            throw_card = index_best_array
     else:
         pass
 
@@ -55,8 +60,18 @@ def best_cards(my_cards, lost_card, bungee_mode, score):
     return array
 
 
-def where_to_get(my_cards, lost_card, bungee_mode, score):
-    pass
+def where_to_get(my_cards, lost_card, bungee_mode, score, index_best_array):
+    t = None
+    if lost_card == index_best_array[0]:
+        return None
+    else:
+        for i in my_cards:
+            if lost_card == i:
+                t = True
+        if t:
+            return False
+
+
 
 
 def if_to_say_bungee(my_cards, lost_card, bungee_mode, score, index_best_array):
