6f2074261d7427507e6ad4c07e846ecc565f20ae
Iftach Shalev <iftachshalev30115@gmail.com>
2020-04-06 17:34:29 +0300

    iftach Algo

--- iftachAlgo.py
+++ iftachAlgo.py
@@ -1,7 +1,6 @@
-from  Maneger import Manager
 
 
-def iftach_algo_simple(my_cards, lucky_card, lost_card, bungee_mode, score):
+def simple(my_cards, lucky_card, lost_card, bungee_mode):
 
     say_bungee = False
     throw_card = []
@@ -14,58 +13,85 @@ def iftach_algo_simple(my_cards, lucky_card, lost_card, bungee_mode, score):
         if card == luc:
             my_cards[i] = 0
 
-    if lost_card == None:
+    index_best_array = best_cards(my_cards)
+
+    if lost_card is None:
+
+        if bungee_mode:
+            is_bungee = if_to_say_bungee(my_cards, lost_card, index_best_array)
+            if not is_bungee:
+                to_quit = True
+            elif is_bungee:
+                say_bungee = True
+            elif is_bungee is None:
+                from_stack = False
+                throw_card = index_best_array
+            user = {
+                'say_bungee': say_bungee,
+                'from_stack': from_stack,
+                'quit': to_quit,
+                'error': error,
+                'throw_cards': throw_card
+            }
+            return user
+
         if sum(my_cards) <= 5:
             say_bungee = True
-            dict = {
-            'say_bungee': say_bungee,
-            'from_stack': from_stack,
-            'quit': to_quit,
-            'error': error,
-            'throw_cards': throw_card
-            }
-            return dict
 
-        throw_card = best_cards(my_cards, lost_card, bungee_mode, score)
-        dict = {
+            user = {
+                'say_bungee': say_bungee,
+                'from_stack': from_stack,
+                'quit': to_quit,
+                'error': error,
+                'throw_cards': throw_card
+            }
+            return user
+
+        throw_card = best_cards(my_cards)
+        user = {
             'say_bungee': say_bungee,
             'from_stack': from_stack,
             'quit': to_quit,
             'error': error,
             'throw_cards': throw_card
         }
-        return dict
 
-    index_best_array = best_cards(my_cards, lost_card, bungee_mode, score)
-    is_bungee = if_to_say_bungee(my_cards, lost_card, bungee_mode, score, index_best_array)
+        return user
+
+    is_bungee = if_to_say_bungee(my_cards, lost_card, index_best_array)
 
     if bungee_mode:
+
         if not is_bungee:
             to_quit = True
         elif is_bungee:
             say_bungee = True
-        if is_bungee == None:
+        if is_bungee is None:
             from_stack = False
             throw_card = index_best_array
+
     else:
-        is_bungee = if_to_say_bungee(my_cards, lost_card, bungee_mode, score, index_best_array)
+
+        is_bungee = if_to_say_bungee(my_cards, lost_card, index_best_array)
         if is_bungee:
             say_bungee = True
-        elif is_bungee == None:
+        elif is_bungee is None:
             from_stack = False
             throw_card = index_best_array
         else:
-            where = where_to_get(my_cards, lost_card, bungee_mode, score, index_best_array)
-            if where == None:
+
+            where = where_to_get(my_cards, lost_card, index_best_array)
+            if where is None:
                 new = []
                 for i in my_cards:
-                    if i != index_best_array[0]:
+                    if i != my_cards[index_best_array[0]]:
                         new.append(i)
-                new_index_best_array = best_cards(new, lost_card, bungee_mode, score)
-                if new_index_best_array[0] < 4:
+                new_index_best_array = best_cards(new)
+                if my_cards[new_index_best_array[0]] < 4:
                     throw_card = index_best_array
                     from_stack = True
                 else:
+
                     throw_card = new_index_best_array
                     from_stack = False
             if not where:
@@ -75,8 +101,18 @@ def iftach_algo_simple(my_cards, lucky_card, lost_card, bungee_mode, score):
                 throw_card = index_best_array
                 from_stack = True
 
+    user = {
+        'say_bungee': say_bungee,
+        'from_stack': from_stack,
+        'quit': to_quit,
+        'error': error,
+        'throw_cards': throw_card
+    }
 
-def best_cards(my_cards, lost_card, bungee_mode, score):
+    return user
+
+
+def best_cards(my_cards):
     max_cards = max(my_cards)
     array = []
     for i, card in enumerate(my_cards):
@@ -85,7 +121,7 @@ def best_cards(my_cards, lost_card, bungee_mode, score):
     return array
 
 
-def where_to_get(my_cards, lost_card, bungee_mode, score, index_best_array):
+def where_to_get(my_cards, lost_card, index_best_array):
     t = None
     if lost_card == index_best_array[0]:
         return t
@@ -100,15 +136,24 @@ def where_to_get(my_cards, lost_card, bungee_mode, score, index_best_array):
         return True
 
 
-def if_to_say_bungee(my_cards, lost_card, bungee_mode, score, index_best_array):
+def if_to_say_bungee(my_cards, lost_card, index_best_array):
     my_sam = sum(my_cards)
+
     if my_sam <= 5:
         bungee = True
-        if my_sam - sum(index_best_array) + lost_card < my_sam:
+        if lost_card is not None:
+            if my_sam - sum(index_best_array) + lost_card < my_sam:
+                bungee = None
+        return bungee
+    elif lost_card is not None:
+        if my_sam - sum(index_best_array) + lost_card <= 5:
             bungee = None
-    elif my_sam - sum(index_best_array) + lost_card <= 5:
-        bungee = []
+            return bungee
+
     else:
         bungee = False
-    return bungee
+        return bungee
 
+
+name = simple([1, 5, 8, 10, 10], 4, 9, False)
+print(name)
