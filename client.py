e093c2cc49928e52a6e4a34dfcf5a63c3864f411
Iftach Shalev <iftachshalev30115@gmail.com>
2020-04-21 19:49:26 +0300

    do nothing

--- client.py
+++ client.py
@@ -1,8 +1,7 @@
 import socket
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
-
+HOST = '127.0.0.1'
+PORT = 65432
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     s.sendall(b'Hello, world')
