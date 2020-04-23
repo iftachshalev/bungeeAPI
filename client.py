90923cab9bc93a4a2d3bbce52f2e11382f1870fd
Iftach Shalev <iftachshalev30115@gmail.com>
2020-04-23 11:55:57 +0300

    do nothing

--- client.py
+++ client.py
@@ -2,11 +2,27 @@ import socket
 
 HOST = '127.0.0.1'
 PORT = 65432
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
 
-print('Received', repr(data))
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+s.connect((HOST, PORT))
 
+data = b""
+
+while True:
+    inp = input("enter txt:")
+    s.sendall(inp.encode())
+    while True:
+        daata = s.recv(1024)
+        if daata[-2] == b"Q":
+            break
+        else:
+            data += daata
+            print(daata)
+    if data == b"quit":
+        print("quit")
+        break
+    # date = s.recv(1024)
+    print('Received:', data)
+
+s.close()
--- server.py
+++ server.py
@@ -1,16 +1,30 @@
 import socket
 
-HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+HOST = '127.0.0.1'
+PORT = 65432
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.bind((HOST, PORT))
+
+s.listen()
+conn, addr = s.accept()
+
+print('Connected by', addr)
+
+while True:
+    data = conn.recv(1024)
+    if data == b"Q":
+        conn.sendall(b"quit")
+        break
+
+    conn.sendall(b"len:   ")
+    conn.sendall(str(len(data)).encode())
+    conn.sendall(b"last:   ")
+    conn.sendall(str(data)[-2].encode())
+    conn.sendall(b"Q")
+
+
+
+conn.close()
+s.close()
