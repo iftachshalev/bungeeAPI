0d860af368a36b391f94f486c143709cc43c381b
Iftach Shalev <iftachshalev30115@gmail.com>
2020-04-30 19:55:40 +0300

    basic working gui with castom class

--- UI/main_kivy.py
+++ UI/main_kivy.py
@@ -6,21 +6,20 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.properties import ObjectProperty
+from kivy.uix.widget import Widget
 
 
-class MyFloatLayout(FloatLayout):
+class MyFloatLayout(Widget):
+    btn_card_1 = ObjectProperty(None)
+    btn_card_2 = ObjectProperty(None)
     def __init__(self, **kwargs):
-        super(FloatLayout, self).__init__(**kwargs)
-    #     self.my_cards = [0, 2, 6, 6, 10]
-    #
-    #     # init socket (connect)
+        super(MyFloatLayout, self).__init__(**kwargs)
+        self.my_card = [1, 2, 3, 4, 6]
+        self.update_cards()
 
-    # def bungee_disable(self, instance):
-    #     if sum(self.my_cards) <= 5:
-    #         instance.disabled = False
-    #     else:
-    #         instance.disabled = True
-
+    def update_cards(self):
+        self.btn_card_1.text = str(self.my_card[0])
+        self.btn_card_2.text = str(self.my_card[1])
 
 class Main_kivyApp(App):
     def build(self):
